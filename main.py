0c5ec81994f0186fdf66cb22bf41544102eec9fc
L0vyR <193486685+L0vyR@users.noreply.github.com>
2025-03-09 09:08:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,6 @@ def get_book_text(file_path):
         file_content = f.read()
         return file_content
 
-#def main():
-#    print(get_book_text("/home/sweery/workspace/github.com/bookbot/books/frankenstein.txt"))
-
 
 print("============ BOOKBOT ============")
 print("Analyzing book found at books/frankenstein.txt...")
